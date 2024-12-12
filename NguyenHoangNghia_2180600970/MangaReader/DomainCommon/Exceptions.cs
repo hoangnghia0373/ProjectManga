3cafe1b1791a90b34d3a10a684003d2a9ee607a9
hoangnghia0373 <hoangnghia0373@gmail.com>
2024-12-12 14:20:00 +0700

    !create project Lab 1.5

---
+++ NguyenHoangNghia_2180600970/MangaReader/DomainCommon/Exceptions.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MangaReader.DomainCommon;
+
+public class NetworkException : Exception
+{
+    public NetworkException(string msg) : base(msg)
+    {
+        
+    }
+}
+
+public class ParseException : Exception
+{
+    
+}
+    
